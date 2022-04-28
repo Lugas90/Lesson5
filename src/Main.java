b50b133b73198282d214a03d7b96f1897a27848f
Lugas90 <Lugas90@mail.ru>
2022-04-28 22:19:21 +0300

    ДЗ урока 5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        int [] array1 = new int[] {1,2,3};
+        double [] array2 = {1.57, 7.654, 9.986};
+        char [] array3 = {'q', 'w', 'e', 'r', 't', 'y'};
+        // Задание 2
+        System.out.println(array1[0] + ", " + array1[1] + ", " + array1[2]);
+        System.out.println(array2[0] + ", " + array2[1] + ", " + array2[2]);
+        System.out.println(array3[0] + ", " + array3[1] + ", " + array3[2] + ", " + array3[3] + ", " + array3[4] + ", " + array3[5]);
+
+        // Задание 3
+        System.out.println(array1[2] + ", " + array1[1] + ", " + array1[0]);
+        System.out.println(array2[2] + ", " + array2[1] + ", " + array2[0]);
+        System.out.println(array3[5] + ", " + array3[4] + ", " + array3[3] + ", " + array3[2] + ", " + array3[1] + ", " + array3[0]);
+
+        // Задание 4
+        for (int i = 0; i < array1.length; i++){
+            if (array1[i] % 2 != 0) {
+                array1[i]++;
+            }
+        }
+        System.out.println(array1[0] + " " + array1[1] + " " + array1[2]);
     }
 }
